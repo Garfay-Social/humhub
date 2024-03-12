b4c37a12b87053dcac251e82b50811e728a5933b
G-a-r-yy <89161975+G-a-r-yy@users.noreply.github.com>
2024-03-11 17:49:12 -0700

    Update WallEntry.php

--- protected/humhub/modules/post/widgets/WallEntry.php
+++ protected/humhub/modules/post/widgets/WallEntry.php
@@ -48,77 +48,11 @@ class WallEntry extends WallStreamEntryWidget
         /** @var Module $module */
         $module = Yii::$app->getModule('post');
 
-        //<?php
-
-/**
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
- */
-
-namespace humhub\modules\post\widgets;
-
-use humhub\modules\content\widgets\stream\WallStreamEntryWidget;
-use humhub\modules\post\models\Post;
-use humhub\modules\post\Module;
-use Yii;
-
-/**
- * @inheritdoc
- */
-class WallEntry extends WallStreamEntryWidget
-{
-    /**
-     * Route to create a content
-     *
-     * @var string
-     */
-    public $createRoute = '/post/post/create-form';
-
-    /**
-     * @inheritdoc
-     */
-    public $editRoute = '/post/post/edit';
-
-    /**
-     * @inheritdoc
-     */
-    public $createFormSortOrder = 100;
-
-    /**
-     * @inheritdoc
-     */
-    public $createFormClass = Form::class;
-
-    /**
-     * @inheritdoc
-     */
-    protected function renderContent()
-    {
-        /** @var Module $module */
-        $module = Yii::$app->getModule('post');
-
-        //$post->load(Yii::$app->request->post(), 'Post');
-        
-    
-
         return $this->render('wallEntry', [
             'post' => $this->model,
             'justEdited' => $this->renderOptions->isJustEdited(), // compatibility for themed legacy views
             'renderOptions' => $this->renderOptions,
-            'starRating' => $post->message,
-            'enableDynamicFontSize' => $module->enableDynamicFontSize
-        ]);
-    }
-}
-
-    
-
-        return $this->render('wallEntry', [
-            'post' => $this->model,
-            'justEdited' => $this->renderOptions->isJustEdited(), // compatibility for themed legacy views
-            'renderOptions' => $this->renderOptions,
-            '
+            'starRating' => $this->message,
             'enableDynamicFontSize' => $module->enableDynamicFontSize
         ]);
     }
