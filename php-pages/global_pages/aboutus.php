9a76847251ed4c4f60f49a0e644040088c24df8a
vidinh1 <118785324+vidinh1@users.noreply.github.com>
2024-03-11 20:53:52 -0400

    about us

--- php-pages/global_pages/aboutus.php
+++ php-pages/global_pages/aboutus.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html>
+<style>
+h2 {
+    font-size: xx-large;
+}
+
+h3{
+    font-size: large;
+}
+</style>
 
 <head>
     <meta charset="UTF-8">
@@ -11,7 +20,12 @@
 	<div class="col-md-12">
      	<div class="panel panel-default">
 			<div class="panel-body">
-            	{{ content }}
+            	
+                <h2> Welcome To Garfay Social! <h2>
+                
+                <h3> Our goal is to provide a place where people and businesses can connect and where customers can find out more information about businesses.
+This site should allow for transparent and constructive interactions between customers and businesses. Businesses will be able to maintain a professional presence, engage with their audience, and gain insights from reviews.
+The platform should ensure security by preserving the confidentiality and integrity of user data. <h3>
                 <h1>Contact Us</h1>
 
      			<form class="form-group" method="post">
@@ -77,10 +91,57 @@ if(isset($_POST['submit'])) {
 
 		// Send 
 		$mail->send();
-		echo 'Message has been sent';
+		
+
+		echo "<div class='notification' id='notification'>Email sent successfully</div>";
 	} catch (Exception $e) {
 		echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 	}
 }
 
 ?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Popup </title>
+    <style>
+       
+        .notification {
+            display: none;
+            position: fixed;
+            bottom: 10px;
+            left: 10px;
+            background-color: white;
+            padding: 45px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            z-index: 9999;
+            font-size: 40px;
+        }
+
+        .notification.error {
+            background-color: #ffcccc; 
+        }
+
+    </style>
+</head>
+<body>
+
+<script>
+    
+    window.onload = function() {
+        var notification = document.getElementById('notification');
+        if (notification) {
+            notification.style.display = 'block';
+            setTimeout(function() {
+                notification.style.display = 'none';
+            }, 5000); 
+
+        }
+    };
+</script>
+
+</body>
+</html>
