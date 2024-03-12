d522eeb4a8c9df18fb9dadd6759bff89a146f2d0
G-a-r-yy <89161975+G-a-r-yy@users.noreply.github.com>
2024-03-11 17:38:48 -0700

    Update WallEntry.php

--- protected/humhub/modules/post/widgets/WallEntry.php
+++ protected/humhub/modules/post/widgets/WallEntry.php
@@ -48,6 +48,9 @@ class WallEntry extends WallStreamEntryWidget
         /** @var Module $module */
         $module = Yii::$app->getModule('post');
 
+        $message = $post->message;
+    
+
         return $this->render('wallEntry', [
             'post' => $this->model,
             'justEdited' => $this->renderOptions->isJustEdited(), // compatibility for themed legacy views
