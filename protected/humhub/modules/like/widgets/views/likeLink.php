4891d8d00bdce5cafe9e9045df0b9bef79192e72
G-a-r-yy <89161975+G-a-r-yy@users.noreply.github.com>
2024-07-01 18:58:44 -0400

    Update likeLink.php

--- protected/humhub/modules/like/widgets/views/likeLink.php
+++ protected/humhub/modules/like/widgets/views/likeLink.php
@@ -8,18 +8,17 @@ humhub\modules\like\assets\LikeAsset::register($this);
 <span class="likeLinkContainer" id="likeLinkContainer_<?= $id ?>">
 
     <?php if (Yii::$app->user->isGuest): ?>
-
-        <?= Html::a(Yii::t('LikeModule.base', 'Like'), Yii::$app->user->loginUrl, ['data-target' => '#globalModal']); ?>
+        <?= Html::a('<i class="fa fa-thumbs-up"></i>', Yii::$app->user->loginUrl, ['data-target' => '#globalModal']); ?>
     <?php else: ?>
-        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $likeUrl ?>" class="like likeAnchor<?= !$canLike ? ' disabled' : '' ?>" style="<?= (!$currentUserLiked) ? '' : 'display:none'?>">
-            <?= Yii::t('LikeModule.base', 'Like') ?>
+        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $likeUrl ?>" class="like likeAnchor<?= !$canLike ? ' disabled' : '' ?><?php echo $currentUserLiked ? ' liked' : ''; ?>" style="<?= (!$currentUserLiked) ? '' : 'display:none'?>">
+            <i class="fa fa-thumbs-up"></i>
         </a>
-        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $unlikeUrl ?>" class="unlike likeAnchor<?= !$canLike ? ' disabled' : '' ?>" style="<?= ($currentUserLiked) ? '' : 'display:none'?>">
-            <?= Yii::t('LikeModule.base', 'Unlike') ?>
+        <a href="#" data-action-click="like.toggleLike" data-action-url="<?= $unlikeUrl ?>" class="unlike likeAnchor<?= !$canLike ? ' disabled' : '' ?><?php echo $currentUserLiked ? '' : ' unliked'; ?>" style="<?= ($currentUserLiked) ? '' : 'display:none'?>">
+            <i class="fa fa-thumbs-up"></i>
         </a>
     <?php endif; ?>
 
-        <!-- Create link to show all users, who liked this -->
+    <!-- Create link to show all users, who liked this -->
     <a href="<?= $userListUrl; ?>" data-target="#globalModal">
         <?php if (count($likes)) : ?>
             <span class="likeCount tt" data-placement="top" data-toggle="tooltip" title="<?= $title ?>">(<?= count($likes) ?>)</span>
