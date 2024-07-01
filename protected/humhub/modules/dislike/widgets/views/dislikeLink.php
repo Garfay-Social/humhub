689ad3a84e2526cc32b54cbf0cc50a8f06a9a8b1
G-a-r-yy <89161975+G-a-r-yy@users.noreply.github.com>
2024-07-01 18:24:45 -0400

    Update dislikeLink.php

--- protected/humhub/modules/dislike/widgets/views/dislikeLink.php
+++ protected/humhub/modules/dislike/widgets/views/dislikeLink.php
@@ -8,18 +8,17 @@ humhub\modules\dislike\assets\DislikeAsset::register($this);
 <span class="dislikeLinkContainer" id="dislikeLinkContainer_<?= $id ?>">
 
     <?php if (Yii::$app->user->isGuest): ?>
-
-        <?= Html::a(Yii::t('DislikeModule.base', 'Dislike'), Yii::$app->user->loginUrl, ['data-target' => '#globalModal']); ?>
+        <?= Html::a('<i class="fa fa-thumbs-down"></i>', Yii::$app->user->loginUrl, ['data-target' => '#globalModal']); ?>
     <?php else: ?>
         <a href="#" data-action-click="dislike.toggleDislike" data-action-url="<?= $dislikeUrl ?>" class="dislike dislikeAnchor<?= !$canDislike ? ' disabled' : '' ?>" style="<?= (!$currentUserDisliked) ? '' : 'display:none'?>">
-            <?= Yii::t('DislikeModule.base', 'Dislike') ?>
+            <i class="fa fa-thumbs-down"></i>
         </a>
         <a href="#" data-action-click="dislike.toggleDislike" data-action-url="<?= $undislikeUrl ?>" class="undislike dislikeAnchor<?= !$canDislike ? ' disabled' : '' ?>" style="<?= ($currentUserDisliked) ? '' : 'display:none'?>">
-            <?= Yii::t('DislikeModule.base', 'Undislike') ?>
+            <i class="fa fa-thumbs-up"></i>
         </a>
     <?php endif; ?>
 
-        <!-- Create link to show all users, who liked this -->
+    <!-- Create link to show all users, who disliked this -->
     <a href="<?= $userListUrl; ?>" data-target="#globalModal">
         <?php if (count($dislikes)) : ?>
             <span class="dislikeCount tt" data-placement="top" data-toggle="tooltip" title="<?= $title ?>">(<?= count($dislikes) ?>)</span>
